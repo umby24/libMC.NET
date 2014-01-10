4a4aaed604f6d37b5419042125a663f64c4dcd85
umby24 <wanderingscripter@gmail.com>
2014-01-10 00:07:22 -0600

    Created Test Client
    Test client gives a brief example of the usage of the library.

---
+++ TestClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using libMC.NET;
+
+namespace TestClient {
+    class Program {
+        static void Main(string[] args) {
+            var MinecraftServer = new Minecraft(args[0], int.Parse(args[1]), "Testbot", "", false);
+            MinecraftServer.Connect();
+
+            MinecraftServer.Message += (sender, message, name) => {
+                Console.WriteLine("<" + name + "> " + message);
+            };
+
+            string command;
+
+            do {
+                command = Console.ReadLine();
+
+                if (command.StartsWith("say ")) 
+                    libMC.NET.Packets.Play.ServerBound.ChatMessage.SendChat(MinecraftServer, command.Substring(4));
+                
+            } while (command != "quit");
+
+            MinecraftServer.Disconnect();
+        }
+    }
+}
